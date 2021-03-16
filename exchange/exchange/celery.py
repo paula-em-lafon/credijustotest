a86a661a0a31af99b0dcca917204f28182630014
Paula Emilia Lafon <paula.em.lafon@gmail.com>
2021-03-16 09:52:45 -0700

    modified celery

--- exchange/exchange/celery.py
+++ exchange/exchange/celery.py
@@ -4,6 +4,10 @@ from celery import Celery
 from celery.schedules import crontab
 from django.conf import settings
 
+import datetime
+import pytz
+nowfun = lambda: datetime.datetime.now(pytz.timezone('America/Mexico_City'))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exchange.settings")
 
@@ -16,15 +20,15 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.beat_schedule = {
     'read_bdm': {
         'task': 'api.tasks.read_bdm',
-        'schedule': crontab(hour=0,minute=0),
+        'schedule': crontab(hour=0,minute=0, nowfun=nowfun),
     },
     'read_dof': {
         'task': 'api.tasks.read_dof',
-        'schedule': crontab(hour=0,minute=0),
+        'schedule': crontab(hour=0,minute=0, nowfun=nowfun),
     },
     'read_fixer': {
         'task': 'api.tasks.read_fixer',
-        'schedule': crontab(hour=0,minute=0),
+        'schedule': crontab(hour=0,minute=0, nowfun=nowfun),
     },
 
 }
