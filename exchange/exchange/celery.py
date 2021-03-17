e75904366f78f275015ab11609ddbfd0d8d10131
Paula Emilia Lafon <paula.em.lafon@gmail.com>
2021-03-17 11:13:19 -0700

    small changes to guarantee latest exchange

--- exchange/exchange/celery.py
+++ exchange/exchange/celery.py
@@ -19,15 +19,15 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.beat_schedule = {
     'read_bdm': {
         'task': 'api.tasks.read_bdm',
-        'schedule': crontab(hour=12,minute=0),
+        'schedule': crontab(hour=12,minute=1),
     },
     'read_dof': {
         'task': 'api.tasks.read_dof',
-        'schedule': crontab(hour=0,minute=0),
+        'schedule': crontab(hour=0,minute=1),
     },
     'read_fixer': {
         'task': 'api.tasks.read_fixer',
-        'schedule': crontab(hour=0,minute=0),
+        'schedule': crontab(hour=0,minute=1),
     },
 
 }
